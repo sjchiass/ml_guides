8be277c6174605216e865533344f6ba2e8807ed8
Sebastien Chiasson <sjchiass@gmail.com>
2018-11-09 07:54:12 -0500

    Add three new games

--- Reinforcement Learning/Beginnings/README.md
+++ Reinforcement Learning/Beginnings/README.md
@@ -16,27 +16,61 @@ Some vocabulary I've learned:
 * Policy: the agent's strategy for using action `a` when confronted with state `s`
 * Value: how the agent values actions; genetic algorithms don't use value functions unlike true reinforcement learning models
 
+## Environments
+
+* CartPole [https://gym.openai.com/envs/CartPole-v1/](https://gym.openai.com/envs/CartPole-v1/)
+* MountainCar [https://gym.openai.com/envs/MountainCar-v0/](https://gym.openai.com/envs/MountainCar-v0/)
+* Pendulum [https://github.com/openai/gym/wiki/Pendulum-v0](https://github.com/openai/gym/wiki/Pendulum-v0)
+* LunarLander [https://github.com/openai/gym/wiki/Leaderboard#lunarlander-v2](https://github.com/openai/gym/wiki/Leaderboard#lunarlander-v2)
+* BipedalWalker [https://github.com/openai/gym/wiki/BipedalWalker-v2](https://github.com/openai/gym/wiki/BipedalWalker-v2)
+
 ## Jupyter Notebooks
 
-### 1 Random_Agents
+The agents I use are all linear models. Cart-pole uses a logistic regression. Multi-output environments use a softmax regression. For extra control the observations can be transformed into polynomials.
+
+### 1 Random Agents
 
 I try out some basic OpenAI Gym code. If you haven't heard of it before, OpenAI Gym is a friendly way to get RL environments (little games) to play with. With a bit of code you can get it to display its games as GIFs, which is the main point of this notebook.
 
-### 2 Modelled_Agents
+### 2 Modelled Agents
 
 This can be skipped. I tried to solve the games with my own ideas. I failed.
 
-### 3 Genetic_Agents
+### 3 Genetic Agents
 
 I finally try a genetic algorithm and have success. I try different configurations to see the effect.
+
+![Demo](./images/genetic_5_parents.gif)
 
 ### 4 MountainCar
 
 I try the genetic algorithm on the Mountain-Car environment, which is alledgedly difficult for some RL models to solve. After some early failures, I got the genetic algorithm to work well with it.
 
-### 5 Modifying Rewards
+![Demo](./images/mountaincar_50.gif)
+
+### 5 Modifying Rewards for cart-pole
 
 I've noticed some of the cart pole agents drift off screen but still win the game. If I add a light penalty for straying off-center will they behave better? Yes.
+
+![Demo](./images/mod_cart_25.gif)
+
+### 6 Pendulum
+
+This game has you trying to balance a pendulum upside down. The GA does alright.
+
+![Demo](./images/pendulum_250.gif)
+
+### 7 Lunar Lander
+
+In this game you try to land a spacecraft on a lunar base. The GA does alright (140) but doesn't solve the game (200).
+
+![Demo](./images/lander_100.gif)
+
+### 8 Bipedal Bot
+
+Teach a robot to walk. The GA makes it skips for a few yards.
+
+![Demo](./images/bipedal_50.gif)
 
 ## An ioslides Presentation
 
--- Reinforcement Learning/Beginnings/agents.py
+++ Reinforcement Learning/Beginnings/agents.py
@@ -1,30 +1,49 @@
 import numpy as np
 import gym
 from IPython.display import HTML
-import PIL.Image
+from PIL import Image,ImageDraw, ImageFont
+from sklearn.preprocessing import PolynomialFeatures
 
 class LinearAgent:
-    def __init__(self, 
-            weights=None, 
-            order=1, 
-            id=None, 
-            pedigree=None, 
-            cartpole=True
+    def __init__(self,
+            weights=None,
+            order=1,
+            id=None,
+            pedigree=None,
+            game="CartPole-v1"
         ):
         # The order of the linear agent
         self.order = order
 
+        # The game
+        self.game = game
+
         # I hardcode the observation and action spaces here
-        if cartpole:
-            self.game = "CartPole-v1"
+        if game == "CartPole-v1":
             self.obs = 4
-            self.ws = 1+order*self.obs
             self.output = 1
-        else:
-            self.game = "MountainCar-v0"
+            #self.ws = 1+order*self.obs
+        elif game == "MountainCar-v0":
             self.obs = 2
             self.output = 3
-            self.ws = self.output*(1+order*self.obs)
+            #self.ws = self.output*(1+order*self.obs)
+        elif game == "Pendulum-v0":
+            self.game = "Pendulum-v0"
+            self.obs = 3
+            self.output = 5
+            #self.ws = self.output*(1+order*self.obs)
+        elif game == "LunarLander-v2":
+            self.game = "LunarLander-v2"
+            self.obs = 8
+            self.output = 4
+            #self.ws = self.output*(1+order*self.obs)
+        else:
+            self.game = "BipedalWalker-v2"
+            self.obs = 24
+            self.output = 20
+
+        self.poly = PolynomialFeatures(degree=order)
+        self.ws = (self.poly.fit_transform(np.zeros((1, self.obs))).shape[1], self.output)
 
         # This segment handles inheriting weights and pedigree
         if weights is None:
@@ -32,51 +51,67 @@ class LinearAgent:
         else:
             self.w = weights
         if id is not None:
-            self.pedigree = self.ws * [str(id)]
+            self.pedigree = self.ws[0] * self.ws[1] * [str(id)]
         elif pedigree is not None:
             self.pedigree = pedigree
         else:
-            self.pedigree = self.ws * [str(-1)]
-        
+            self.pedigree = self.ws[0] * self.ws[1] * [str(-1)]
+
     def predict(self, x):
-        Y = []
+        x = x.reshape(1, -1)
 
-        for i in range(self.output):
-            # Intercept
-            z = self.w[i*self.obs]
-
-            # Orders
-            for j in range(self.order):
-                for o in range(self.obs):
-                    z += self.w[i*self.obs+1+j+o]*x[o]**(j+1)
-            Y.append(z)
+        Y = np.dot(self.poly.transform(x), self.w)
 
         if self.game == "CartPole-v1": # sigmoid
             z = Y[0]
             a = 1 / (1 + np.exp(-z))
-            return int(round(a))        
-        else: # softmax
-            z_num = sum([np.exp(i) for i in Y])
-            a = [np.exp(i)/z_num for i in Y]
-            return np.argmin(a)
+            return int(np.round(a))
+        elif self.game in ["MountainCar-v0", "LunarLander-v2"]: # cheap softmax
+            return int(np.argmax(Y, axis=1))
+        elif self.game == "Pendulum-v0":
+            return [np.argmax(Y, axis=1) - 2]
+        else:
+            Y = Y.flatten()
+            hip1 = float(np.argmax(Y[0:4]))/2 -1
+            knee1 = float(np.argmax(Y[5:9]))/2 -1
+            hip2 = float(np.argmax(Y[10:14]))/2 -1
+            knee2 = float(np.argmax(Y[15:19]))/2 -1
+            return [hip1, knee1, hip2, knee2]
+
 
     def render(self, filename, episodes=5, limit=1000):
         # Prepend the sub-folder
         filename = "./images/"+filename
+
+        fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 32)
 
         env = gym.make(self.game)
 
         frames = []
         for e in range(episodes):
             observation = env.reset()
+            total_reward = 0
             for t in range(limit):
                 # Skip some frames
-                if t % 10 == 0:
-                    frames.append(PIL.Image.fromarray(env.render(mode = 'rgb_array'), "RGB"))
+                if t % 5 == 0:
+                    frames.append(Image.fromarray(env.render(mode = 'rgb_array'), "RGB"))
+                    # Draw the episode number
+                    ImageDraw.Draw(frames[-1]).text((0,frames[-1].size[1]-64), f"episode  {e+1}",
+                        font=fnt, fill=(0,0,0,0))
+                    # Draw the timestep number
+                    ImageDraw.Draw(frames[-1]).text((0,frames[-1].size[1]-32), f"timestep {t}",
+                        font=fnt, fill=(0,0,0,0))
+                    # Draw the total reward
+                    ImageDraw.Draw(frames[-1]).text((frames[-1].size[0]-128,frames[-1].size[1]-32),
+                        str(int(total_reward)), font=fnt, fill=(0,0,0,0))
                 action = self.predict(observation)
                 observation, reward, done, info = env.step(action)
+                total_reward += reward
                 if done:
                     break
+            # Triple the last frame
+            frames.append(frames[-1])
+            frames.append(frames[-1])
         env.close()
 
         # Resize the frames
@@ -84,7 +119,7 @@ class LinearAgent:
             f.thumbnail(size=[300,200])
 
         # Save the GIF
-        frames[0].save(filename, format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
+        frames[0].save(filename, format='GIF', append_images=frames[1:], save_all=True, duration=75, loop=0)
 
         # Return the filename to be input in HTML()
         return(filename)
